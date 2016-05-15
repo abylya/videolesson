4c77186a500e88e4d54b0c45c9a280da4ec8232c
abdyla <abdyla0303@yandex.ru>
2016-05-15 11:23:26 +0300

    1com v f1

--- src/main/java/mavenproject/Hello.java
+++ src/main/java/mavenproject/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Автоматически созданная заглушка метода
 System.out.println("Hello");
+System.out.println("Hellof1");
 	}
 
 }
